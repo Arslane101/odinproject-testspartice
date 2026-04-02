3a1f748136e2d7bc768bfd3f426760a39eef3cf7
Arslane101 <rahalabdelhamid@gmail.com>
2026-04-02 17:49:18 +0100

    Finished half of the project

--- input.js
+++ input.js
@@ -1,4 +1,38 @@
-function sum(a,b){
-    return a+b
+//Capitalize function
+function capitalize(text){
+    return text[0].toUpperCase() + text.slice(1)
 }
-export default sum
+
+//reverse 
+function reverseString(text){
+    let reverse = []
+    for (let i=0;i<text.length;i++){
+        reverse[i]= text[text.length-i-1]
+    }
+    console.log(reverse)
+    return reverse.join("")
+}
+
+const Calculator = (() => {
+    let result;
+    function add(a,b){
+        result = a+b
+        return result
+    }
+    function substract(a,b){
+        result = a-b
+        return result
+    }
+    function multiply(a,b){
+        result = a*b
+        return result
+    }
+    function divide(a,b){
+        result = a/b
+        return result
+    }
+    return {add,divide,multiply,substract}
+
+})()
+
+export default {capitalize,reverseString,Calculator}
--- input.test.js
+++ input.test.js
@@ -1,5 +1,23 @@
-import sum from './input'
+import expect from 'expect'
+import functions from './input'
 
-test('adds 1+2 to equal 3',() => {
-    expect(sum(1,2)).toBe(3)
+test('arslane => Arslane',() => {
+    expect(functions.capitalize("arslane")).toBe("Arslane")
+})
+
+test('arslane => enalsra',() => {
+    expect(functions.reverseString("arslane")).toBe('enalsra')
+})
+
+test ('3+2 =5',() => {
+    expect(functions.Calculator.add(3,2)).toBe(5)
+})
+test ('3x2 = 6',() => {
+    expect(functions.Calculator.multiply(3,2)).toBe(6)
+})
+test ('3/2 = 1.5',() => {
+    expect(functions.Calculator.divide(3,2)).toBe(1.5)
+})
+test ('3-2 =1',() => {
+    expect(functions.Calculator.substract(3,2)).toBe(1)
 })
