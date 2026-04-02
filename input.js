1bb327022c70ec0445a582bf49d25addec28503d
Arslane101 <rahalabdelhamid@gmail.com>
2026-04-02 18:29:34 +0100

    Done

--- input.js
+++ input.js
@@ -12,7 +12,7 @@ function reverseString(text){
     console.log(reverse)
     return reverse.join("")
 }
-
+//Calculator
 const Calculator = (() => {
     let result;
     function add(a,b){
@@ -35,4 +35,36 @@ const Calculator = (() => {
 
 })()
 
-export default {capitalize,reverseString,Calculator}
+//cipher
+function caesarCipher(text,factor){
+    const alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
+    const uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('')
+
+    let result = []
+    for (let i=0;i<text.length;i++){
+      if(alphabet.indexOf(text[i]) !== -1 ){
+        if(alphabet.indexOf(text[i])+factor >= alphabet.length) result[i]= alphabet[alphabet.indexOf(text[i])+factor-alphabet.length]
+        else result[i]= alphabet[alphabet.indexOf(text[i])+factor]
+      } 
+      else if(uppercase.indexOf(text[i]) !== -1 ){
+        if(uppercase.indexOf(text[i])+factor >= uppercase.length) result[i]= uppercase[uppercase.indexOf(text[i])+factor-uppercase.length]
+        else result[i]= uppercase[uppercase.indexOf(text[i])+factor]
+      }
+      else result[i] = text[i]
+    }
+    console.log(result)
+    return result.join("")
+}
+
+const AnalyzeArray = (array) => {
+  const sortedArr = array.sort((a, b) => a - b);
+  const average = array.reduce((acc, curr) => acc + curr, 0) / array.length;
+  const length = array.length;
+  const min = sortedArr[0];
+  const max = sortedArr[sortedArr.length -1];
+
+  const result = { average, min, max, length };
+  return result;
+};
+export default {capitalize,reverseString,Calculator,caesarCipher,AnalyzeArray}
+
--- input.test.js
+++ input.test.js
@@ -21,3 +21,20 @@ test ('3/2 = 1.5',() => {
 test ('3-2 =1',() => {
     expect(functions.Calculator.substract(3,2)).toBe(1)
 })
+
+test('xyz => abc',() => {
+    expect(functions.caesarCipher('xyz',3)).toBe("abc")
+})
+
+test('Hello, World! => Khoor, Zruog!',() => {
+    expect(functions.caesarCipher('Hello, World!',3)).toBe("Khoor, Zruog!")
+})
+
+test('Array',() => {
+    expect(functions.AnalyzeArray([1,8,3,4,2,6])).toEqual({
+    average: 4,
+    min: 1,
+    max: 8,
+    length: 6,
+  })
+})
